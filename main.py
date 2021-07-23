6088ad6d951fdc2161f8c57c7c25c1380ee83a86
Dynìstriøs <73307726+Dynistrios@users.noreply.github.com>
2021-07-24 00:59:24 +0200

    update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def on_message(message):
         
         repond = newmsg.lower()
 
-        url = f"https://api.affiliateplus.xyz/api/chatbot?message={repond}&botname=Koneko_NAME&ownername=Quincy_NAME&user={message.author.name}"
+        url = f"Your api"
         
         response = requests.get(url, verify=True)
         
@@ -44,4 +44,4 @@ async def on_message(message):
         
         
 
-bot.run('Ton token')
+bot.run('Your token')
